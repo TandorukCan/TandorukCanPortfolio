5acc1e217986ce32aa35bdb59510616068b86865
TandorukCan <tandorukcan@gmail.com>
2024-10-21 12:40:58 +0300

    added the documents that are included in Bootstrapmade Pro

--- website/forms/contact.php
+++ website/forms/contact.php
@@ -17,21 +17,18 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'tandorukcan@gmail.com',
     'password' => 'klch hxei vblj shuk',
     'port' => '587'
   );
-
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
